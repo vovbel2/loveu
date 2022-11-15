e90c1aaf646c287ed18ac6201200822223eb5e17
vovbel2 <vovbel2vovbel2@yandex.ru>
2022-11-15 18:40:35 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function toggle(){
+    var date = document.querySelector('input[type="date"]');
+    //date.value = '2021-11-21';
+    if (date.value === '2021-11-21') {
+        var blur = document.getElementById('blur');
+        blur.classList.toggle('active');
+        var popup = document.getElementById('popup');
+        popup.classList.toggle('active');
+        document.getElementById('body').style['background'] = "transparent";
+    }
+}
